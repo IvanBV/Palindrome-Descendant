38836800442b1d4f12f431b41e6168a02dccb6ca
Ivan <iv.bondare@gmail.com>
2020-08-14 18:19:57 +0300

    Initial commit

---
+++ PalindromeDescendant/PalindromeDescendant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace PalindromeDescendant
+{
+    public class PalindromeDescendant
+    {
+        public static bool CheckPalindromeDescendant(int num) 
+        {
+            var numberString = num.ToString();
+            
+            if (numberString.Length % 2 == 1)
+                return false;
+
+            if (IsPalindrome(num))
+                return true;
+
+            // Get descendant
+            return CheckPalindromeDescendant(GetDescandant(num));
+        }
+
+        public static bool IsPalindrome(int num)
+        {
+            var numberString = num.ToString();
+
+            for(int i = 0; i < numberString.Length / 2; i++)
+            {
+                if (numberString[i] != numberString[numberString.Length - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static int GetDescandant(int num)
+        {
+            var numberString = num.ToString();
+
+            // Force check to intentionally fail first test (described in PalindromeDescendant_Tests.cs)
+            if (numberString.Length <= 2)
+                return 0;
+
+            var descendant = new StringBuilder();
+            for(int i = 0; i < numberString.Length; i += 2)
+            {
+                descendant.Append(char.GetNumericValue(numberString[i]) + char.GetNumericValue(numberString[i + 1]));
+            }
+            return Convert.ToInt32(descendant.ToString());
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Palindrome Descendant
+Palindrome Descendant task from edabit.com
+
+https://edabit.com/challenge/PvCD5nSYy3Dnvnfcq
+
+A number may not be a palindrome, but its descendant can be. A number's direct child is created by summing each pair of adjacent digits to create the digits of the next number.
+
+For instance, 123312 is not a palindrome, but its next child 363 is, where: 3 = 1 + 2; 6 = 3 + 3; 3 = 1 + 2.
+
+Create a function that returns true if the number itself is a palindrome or any of its descendants down to 2 digits (a 1-digit number is trivially a palindrome).
